5e25cb3088419b797c34a6f9526c27d7143dd68d
Ben Hunter <code@benhunter.me>
2023-02-22 17:20:37 -0600

    Improve output

--- git-status-directories.sh
+++ git-status-directories.sh
@@ -6,10 +6,12 @@
 for directory in */; do
     cd "$directory"
     # Check the status of the directory using Git
-    git_status=$(git status --porcelain)
+    git_status=$(git status --porcelain 2>/dev/null)
     # If there are changes, output the directory path
     if [ -n "$git_status" ]; then
-        echo "$directory has changes: $git_status"
+        echo "$directory has changes:"
+	echo $git_status
+	echo
     fi
     cd ..
 done
