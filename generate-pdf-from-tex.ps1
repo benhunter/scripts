159479b906ea2231ffbe639972a850c06492bc18
Ben Hunter <code@benhunter.me>
2023-06-19 01:05:48 -0500

    Update generate-pdf-from-tex.ps1

--- generate-pdf-from-tex.ps1
+++ generate-pdf-from-tex.ps1
@@ -1,2 +1,3 @@
 docker run -i --rm --name latex -v ${PWD}:/usr/src/app -w /usr/src/app texlive/texlive:latest pdflatex -shell-escape week_8.tex
+# Example .tex in mcso-ala-matlab/week_8/week_8.tex
 # ${PWD} for powershell, $PWD for bash 
