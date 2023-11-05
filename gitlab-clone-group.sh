a7146866aa0a0045b9ce9fea858d980011b35183
Ben Hunter <code@benhunter.me>
2023-11-05 14:18:55 -0600

    WIP gitlab-clone-group.sh - use the python script instead.

--- gitlab-clone-group.sh
+++ gitlab-clone-group.sh
@@ -1,5 +1,8 @@
 #! /bin/zsh
 
+echo "TODO: this script is WIP"
+exit 1
+
 usage() {
   echo "Usage: $0 <GROUP_NAME>"
 }
@@ -29,16 +32,23 @@ PROJECTS=$(glab api groups/$1/projects | jq -c ".[]")
 # echo $PROJECTS | jq -r '.[].web_url' | xargs -n1 -I {} echo "git clone --recurse-submodules {} $1"
 
 echo $PROJECTS | while IFS= read -r PROJECT; do
-  echo PROJECT: $PROJECT
+  # get the project.name from jq
+  PROJECT_NAME=$(echo $PROJECT | jq -r '.name')
+  echo "PROJECT_NAME: $PROJECT_NAME"
   URL=$(echo $PROJECT | jq -r '.web_url')
-  DIRECTORY=$1/$(echo $PROJECT | jq -r '.name')
-  # echo $URL $DIRECTORY
-  git clone --recurse-submodules $URL $DIRECTORY
+  DIRECTORY=$1/$PROJECT_NAME
+  echo "  git clone $URL $DIRECTORY"
+  # git clone --recurse-submodules $URL $DIRECTORY
 done
 
 SUBGROUPS=$(glab api groups/$1/subgroups | jq -c ".[]")
 
 echo $SUBGROUPS | while IFS= read -r GROUP; do
-  echo GROUP: $GROUP
-  echo TODO: clone subgroups
+  echo "TODO looks like this loop breaks when newline are in the string"
+  echo "GROUP: $GROUP"
+  # GROUP_NAME=$(echo $GROUP | jq -r '.name')
+  # echo "GROUP_NAME: $GROUP_NAME"
+  echo "clone subgroups"
+  gitlab-clone-group.sh $GROUP
 done
+
