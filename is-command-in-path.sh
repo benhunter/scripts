188f0597503c11f32984d70dea33b6e51e0f6dce
Ben Hunter <code@benhunter.me>
2024-06-01 13:11:05 -0500

    Less lines

--- is-command-in-path.sh
+++ is-command-in-path.sh
@@ -3,8 +3,7 @@
 # Check if a command exists in $PATH.
 
 COMMAND='cargo'
-if ! type "$COMMAND" &> /dev/null
-then
+if ! type "$COMMAND" &> /dev/null; then
   echo "$COMMAND" not found in path
 else
   echo "$COMMAND" found
